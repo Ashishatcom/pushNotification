8c32b18e54a476f440d8ceeeb55955f41654793e
Ashish <ashish.soni@cipherventurecapital.com>
2021-06-26 14:28:53 +0530

    firsts1 commit

--- webpush.js
+++ webpush.js
@@ -1,9 +1,11 @@
 const webpush = require('web-push');
 
+// let vapidKeys = webpush.generateVAPIDKeys();
+// console.log(vapidKeys);
 // VAPID keys should only be generated only once.
 const vapidKeys = {
-  publicKey: 'BBZY7Q3KEtZArAAWMLi_qzWHbH4vAoqPpIXnRhmlUaw0PVs1Kt_2fgLhuaVI5i8MWASBKx3d6W6UoH2U3qChw9U',
-  privateKey: 'CZtf_JUxmXkCKbzwaKedPPO9BFC99U2rk-GUYDbYAa8'
+  publicKey: 'BDBnAvs9T9nz4BYNBg7n-A44I0wBwTTN8oF1Q8hpJwglpFF9AZspnKJKu46_9AYnSIEojYJf0NlF5jevVpL_d8I',
+  privateKey: 'rK9c4FIjr_uRfw-GS8zbPuBBUU0kzThTxxfdlJctZ8w'
 };
 
 webpush.setVapidDetails(
@@ -14,11 +16,13 @@ webpush.setVapidDetails(
 
 // This is the same output of calling JSON.stringify on a PushSubscription
 const pushSubscription = {
-  "endpoint": "https://fcm.googleapis.com/fcm/send/fxOAFeGFteQ:APA91bG9cZhlZFU0L_32GumZZNAYzZy1v4tHygFIE26EqCbkrxCr9iEBFCYhhTcxsg1UrsyQuettFJ3xQtrY6ijyIoQxib5Mfhb-VyiK1IGJ1vLRQgjfb0sX5LgJ8TPOoBnIw7jo5rwo",
-  "expirationTime": null,
-  "keys": {
-    "p256dh": "BFtsmY31kTnGkCv_iWVLrgnhGT29xxkLx38rBcop1lW_5Bbj2eblrnZMHsZ2NKHEhDeRzdUd7yyXiHx4lkyZSJs",
-    "auth": "8ncjU_NytUiV71P95GZnQg"
+  
+    "endpoint": "https://fcm.googleapis.com/fcm/send/fxOAFeGFteQ:APA91bG9cZhlZFU0L_32GumZZNAYzZy1v4tHygFIE26EqCbkrxCr9iEBFCYhhTcxsg1UrsyQuettFJ3xQtrY6ijyIoQxib5Mfhb-VyiK1IGJ1vLRQgjfb0sX5LgJ8TPOoBnIw7jo5rwo",
+    "expirationTime": null,
+    "keys": {
+      "p256dh": "BFtsmY31kTnGkCv_iWVLrgnhGT29xxkLx38rBcop1lW_5Bbj2eblrnZMHsZ2NKHEhDeRzdUd7yyXiHx4lkyZSJs",
+      "auth": "8ncjU_NytUiV71P95GZnQg"
+    
   }
 }
 
