00e59721e3d2e579aaa09c28312cc5155044f52d
Ashish <ashish.soni@cipherventurecapital.com>
2021-06-26 14:21:51 +0530

    firsts commit

--- webpush.js
+++ webpush.js
@@ -2,8 +2,7 @@ const webpush = require('web-push');
 
 // VAPID keys should only be generated only once.
 const vapidKeys = {
-  publicKey:
-    'BBZY7Q3KEtZArAAWMLi_qzWHbH4vAoqPpIXnRhmlUaw0PVs1Kt_2fgLhuaVI5i8MWASBKx3d6W6UoH2U3qChw9U',
+  publicKey: 'BBZY7Q3KEtZArAAWMLi_qzWHbH4vAoqPpIXnRhmlUaw0PVs1Kt_2fgLhuaVI5i8MWASBKx3d6W6UoH2U3qChw9U',
   privateKey: 'CZtf_JUxmXkCKbzwaKedPPO9BFC99U2rk-GUYDbYAa8'
 };
 
@@ -15,11 +14,13 @@ webpush.setVapidDetails(
 
 // This is the same output of calling JSON.stringify on a PushSubscription
 const pushSubscription = {
-  endpoint: '.....',
-  keys: {
-    auth: '.....',
-    p256dh: '.....'
+  "endpoint": "https://fcm.googleapis.com/fcm/send/fxOAFeGFteQ:APA91bG9cZhlZFU0L_32GumZZNAYzZy1v4tHygFIE26EqCbkrxCr9iEBFCYhhTcxsg1UrsyQuettFJ3xQtrY6ijyIoQxib5Mfhb-VyiK1IGJ1vLRQgjfb0sX5LgJ8TPOoBnIw7jo5rwo",
+  "expirationTime": null,
+  "keys": {
+    "p256dh": "BFtsmY31kTnGkCv_iWVLrgnhGT29xxkLx38rBcop1lW_5Bbj2eblrnZMHsZ2NKHEhDeRzdUd7yyXiHx4lkyZSJs",
+    "auth": "8ncjU_NytUiV71P95GZnQg"
   }
-};
+}
+
 
 webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
